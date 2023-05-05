0fc275c55ebc2829848f83cc886c6889d37ba882
nunocravino <nuno.cravino@ophis.icu>
2023-05-05 13:01:27 +0100

    Add manual list based on cached requests. Regen

--- scripts/process_doh_list.sh
+++ scripts/process_doh_list.sh
@@ -6,6 +6,10 @@ sed --version >> /dev/null || { echo "Please install sed" ; exit 1; }
 echo "Processing..."
 
 echo "/ip firewall address-list" > mikrotik_doh_list_commands.txt
-cat iplist.txt | grep -v '#' | grep '\S' | sed 's/\s\(\S\)/\1/' | xargs -I% echo 'add address=% list="DoH Servers"' >> mikrotik_doh_list_commands.txt
+cat iplist.txt | grep -v '#' | grep '\S' | sed 's/\s\(\S\)/\1/' > full_list.txt
+cat mylist.txt | grep -v '#' >> full_list.txt
+
+cat full_list.txt | sort -u | xargs -I% echo 'add address=% list="DoH Servers"' >> mikrotik_doh_list_commands.txt
+
 
 echo "All done"
