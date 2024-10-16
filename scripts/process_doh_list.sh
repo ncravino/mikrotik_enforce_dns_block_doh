b08df12509e4aed6f3f3f56e1a74d7e6a54ee56c
tunelessly <9685607+tunelessly@users.noreply.github.com>
2024-10-16 20:59:58 +0100

    Updated data source. Updated scripts in accordance.

--- scripts/process_doh_list.sh
+++ scripts/process_doh_list.sh
@@ -2,14 +2,16 @@
 
 grep --version >> /dev/null || { echo "Please install grep" ; exit 1; }
 sed --version >> /dev/null || { echo "Please install sed" ; exit 1; }
+gawk --version >> /dev/null || { echo "Please install awk" ; exit 1; }
 
 echo "Processing..."
 
-echo "/ip firewall address-list" > mikrotik_doh_list_commands.txt
-cat iplist.txt | grep -v '#' | grep '\S' | sed 's/\s\(\S\)/\1/' > full_list.txt
-cat mylist.txt | grep -v '#' >> full_list.txt
+cat iplist.txt | grep -v '^\s*#' | sed 's/\s\(\S\)/\1/' | gawk '{print $1}' > full_list.txt
+cat mylist.txt | grep -v '^\s*#' | sed 's/\s\(\S\)/\1/' | gawk '{print $1}' >> full_list.txt
+gawk -i inplace 'FNR==1{delete a} !a[$0]++' full_list.txt
 
+echo "/ip firewall address-list remove [find where list="DoH Servers"]" > mikrotik_doh_list_commands.txt
+echo "/ip firewall address-list" >> mikrotik_doh_list_commands.txt
 cat full_list.txt | sort -u | xargs -I% echo 'add address=% list="DoH Servers"' >> mikrotik_doh_list_commands.txt
-
 
 echo "All done"
