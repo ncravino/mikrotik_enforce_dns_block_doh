f9cb8dd738a75730df247e3cab5bd3636f9175dc
tunelessly <9685607+tunelessly@users.noreply.github.com>
2024-10-16 22:40:54 +0100

    Removed unneeded sed stage

--- scripts/process_doh_list.sh
+++ scripts/process_doh_list.sh
@@ -6,8 +6,8 @@ gawk --version >> /dev/null || { echo "Please install awk" ; exit 1; }
 
 echo "Processing..."
 
-cat iplist.txt | grep -v '^\s*#' | sed 's/\s\(\S\)/\1/' | gawk '{print $1}' > full_list.txt
-cat mylist.txt | grep -v '^\s*#' | sed 's/\s\(\S\)/\1/' | gawk '{print $1}' >> full_list.txt
+cat iplist.txt | grep -v '^\s*#' | gawk '{print $1}' > full_list.txt
+cat mylist.txt | grep -v '^\s*#' | gawk '{print $1}' >> full_list.txt
 gawk -i inplace 'FNR==1{delete a} !a[$0]++' full_list.txt
 
 echo "/ip firewall address-list remove [find where list="DoH Servers"]" > mikrotik_doh_list_commands.txt
